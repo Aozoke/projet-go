c86add7cd5052fb580e0ab373b896674e2d707ad
Aozoke <typhaine.lfvr@gmail.com>
2026-07-17 18:03:48 +0200

    engine

--- engine_test.go
+++ engine_test.go
@@ -15,3 +15,19 @@ func TestNewEngine(t *testing.T) {
 		t.Fatal("expected state, got nil")
 	}
 }
+
+func TestEngineSet(t *testing.T) {
+	engine := NewEngine()
+
+	//SET écrire / stocker une valeur. Stocke la valeur matt dans la clé name
+	//On demande au moteur de stocker la valeur matt dans la clé name.
+	//Dans le moteur, mets name -> matt
+	engine.Set("name", "matt")
+
+	//state, c’est l’état actuel de la base en mémoire.
+	//regarder dans la map si la valeur a bien été stockée.
+	//Si ce qu’il y a dans state["name"] n’est pas matt, le test échoue.
+	if engine.state["name"] != "matt" {
+		t.Fatalf("expected matt, got %s", engine.state["name"])
+	}
+}
